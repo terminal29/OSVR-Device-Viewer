97c57f684efe7a554e07e139d7588032ae0bbb01
Christoph Haag <haagch@frickel.club>
2017-07-04 15:43:55 +0200

    qmake project and build fixes for linux

--- QRefreshButton.h
+++ QRefreshButton.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "C:\Qt\Qt5.6.2\5.6\msvc2015_64\include\QtWidgets\qpushbutton.h"
+#include <QtWidgets/qpushbutton.h>
 #include "QPathViewer.h"
 class QRefreshButton : public QPushButton
 {
